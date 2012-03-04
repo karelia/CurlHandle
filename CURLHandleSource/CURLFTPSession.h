bf3e35badcaefdfca6685f987522bcf5ae0ac033
Mike Abdullah <me@mikeabdullah.net>
2012-03-04 18:26:54 +0000

    First crack at providing a specialised FTP interface.

---
+++ CURLHandleSource/CURLFTPSession.h
@@ -0,0 +1,41 @@
+//
+//  CURLFTPSession.h
+//  CURLHandle
+//
+//  Created by Mike Abdullah on 04/03/2012.
+//  Copyright (c) 2012 Karelia Software. All rights reserved.
+//
+
+#import <CURLHandle/CURLHandle.h>
+
+
+@protocol CURLFTPSessionDelegate;
+
+
+@interface CURLFTPSession : NSObject <CURLHandleDelegate>
+{
+  @private
+    CURLHandle          *_handle;
+    NSURLRequest        *_request;
+    NSURLCredential     *_credential;
+    
+    id <CURLFTPSessionDelegate> _delegate;
+}
+
+// Returns nil if not a supported FTP URL
+- (id)initWithRequest:(NSURLRequest *)request;
+
+- (void)useCredential:(NSURLCredential *)credential;
+
+- (BOOL)createFileAtPath:(NSString *)path contents:(NSData *)data permissions:(NSNumber *)permissions error:(NSError **)error;
+- (BOOL)createDirectoryAtPath:(NSString *)path error:(NSError **)error;
+- (BOOL)removeFileAtPath:(NSString *)path error:(NSError **)error;
+
+@property(nonatomic, assign) id <CURLFTPSessionDelegate> delegate;
+
+@end
+
+
+@protocol CURLFTPSessionDelegate <NSObject>
+- (void)FTPSession:(CURLFTPSession *)session didReceiveDebugInfo:(NSString *)info ofType:(curl_infotype)type;
+@end
---
+++ CURLHandleSource/CURLFTPSession.m
@@ -0,0 +1,118 @@
+//
+//  CURLFTPSession.m
+//  CURLHandle
+//
+//  Created by Mike Abdullah on 04/03/2012.
+//  Copyright (c) 2012 Karelia Software. All rights reserved.
+//
+
+#import "CURLFTPSession.h"
+
+
+@implementation CURLFTPSession
+
+- (id)initWithRequest:(NSURLRequest *)request;
+{
+    NSParameterAssert(request);
+    
+    if (self = [self init])
+    {
+        if (![[[request URL] scheme] isEqualToString:@"ftp"])
+        {
+            [self release]; return nil;
+        }
+        _request = [request copy];
+        
+        _handle = [[CURLHandle alloc] init];
+        [_handle setDelegate:self];
+        if (!_handle)
+        {
+            [self release];
+            return nil;
+        }
+    }
+    
+    return self;
+}
+
+- (void)dealloc
+{
+    [_handle release];
+    [_request release];
+    [_credential release];
+    
+    [super dealloc];
+}
+
+- (void)useCredential:(NSURLCredential *)credential
+{
+    [_credential release]; _credential = [credential retain];
+    
+    [_handle setString:[credential user] forKey:CURLOPT_USERNAME];
+    [_handle setString:[credential password] forKey:CURLOPT_PASSWORD];
+}
+
+- (NSMutableURLRequest *)newMutableRequestWithPath:(NSString *)path isDirectory:(BOOL)isDirectory;
+{
+    NSMutableURLRequest *request = [_request mutableCopy];
+    [request setURL:[[request URL] URLByAppendingPathComponent:path isDirectory:isDirectory]];
+    
+    return request;
+}
+
+- (BOOL)createFileAtPath:(NSString *)path contents:(NSData *)data permissions:(NSNumber *)permissions error:(NSError **)error;
+{
+    NSMutableURLRequest *request = [self newMutableRequestWithPath:path isDirectory:NO];
+    [request setHTTPBody:data];
+    
+    BOOL result = [_handle loadRequest:request error:error];
+    [request release];
+    
+    return result;
+}
+
+- (BOOL)createDirectoryAtPath:(NSString *)path error:(NSError **)error;
+{
+    // Navigate to the directory above the one to be created
+    // CURLOPT_NOBODY stops libcurl from trying to list the directory's contents
+    NSMutableURLRequest *request = [self newMutableRequestWithPath:[path stringByDeletingLastPathComponent] isDirectory:YES];
+    [_handle setStringOrNumberObject:[NSNumber numberWithBool:YES] forKey:CURLOPT_NOBODY];
+    
+    // Custom command to delete the file once we're in the correct directory
+    // CURLOPT_PREQUOTE does much the same thing, but sometimes runs the delete command twice in my testing
+    [_handle setStringOrNumberObject:[NSArray arrayWithObject:[@"MKD " stringByAppendingString:[path lastPathComponent]]]
+                                    forKey:CURLOPT_POSTQUOTE];
+    
+    
+    BOOL result = [_handle loadRequest:request error:error];
+    [request release];
+    return result;
+}
+
+- (BOOL)removeFileAtPath:(NSString *)path error:(NSError **)error;
+{
+    // Navigate to the directory containing the file
+    // CURLOPT_NOBODY stops libcurl from trying to list the directory's contents
+    NSMutableURLRequest *request = [self newMutableRequestWithPath:[path stringByDeletingLastPathComponent] isDirectory:YES];
+    [_handle setStringOrNumberObject:[NSNumber numberWithBool:YES] forKey:CURLOPT_NOBODY];
+    
+    // Custom command to delete the file once we're in the correct directory
+    // CURLOPT_PREQUOTE does much the same thing, but sometimes runs the delete command twice in my testing
+    [_handle setStringOrNumberObject:[NSArray arrayWithObject:[@"DELE " stringByAppendingString:[path lastPathComponent]]]
+                                    forKey:CURLOPT_POSTQUOTE];
+    
+    BOOL result = [_handle loadRequest:request error:error];
+    [request release];
+    return result;
+}
+
+#pragma mark Delegate
+
+@synthesize delegate = _delegate;
+
+- (void)handle:(CURLHandle *)handle didReceiveDebugInformation:(NSString *)string ofType:(curl_infotype)type;
+{
+    [[self delegate] FTPSession:self didReceiveDebugInfo:string ofType:type];
+}
+
+@end
