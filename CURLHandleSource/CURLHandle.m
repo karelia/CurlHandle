09641d7ed0a11870fb6fcfd555216500e85b7710
Mike Abdullah <me@mikeabdullah.net>
2012-09-01 20:30:55 +0100

    Don't set CURLOPT_FTP_RESPONSE_TIMEOUT.

--- CURLHandleSource/CURLHandle.m
+++ CURLHandleSource/CURLHandle.m
@@ -340,7 +340,8 @@ int curlDebugFunction(CURL *mCURL, curl_infotype infoType, char *info, size_t in
         //LOAD_REQUEST_SET_OPTION(CURLOPT_TIMEOUT, timeout);
         
         // Make FTP response time shorter so that when faced with a server which turns out not to receive EPSV connections, can fall back to PASV in time
-        LOAD_REQUEST_SET_OPTION(CURLOPT_FTP_RESPONSE_TIMEOUT, 0.5 * timeout);
+        // It seems that on OS X 10.6, this behaves as the maximum time a transfer can take, likely killing the connection for large files, so don't want it. Not supporting EPSV at present anyhow
+        //LOAD_REQUEST_SET_OPTION(CURLOPT_FTP_RESPONSE_TIMEOUT, 0.5 * timeout);
         
 
         
