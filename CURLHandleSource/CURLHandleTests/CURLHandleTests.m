681613248f0136ca33ef0611989e759c77187011
Sam Deane <sam@elegantchaos.com>
2012-09-20 15:44:51 +0100

    added a simple download test

--- CURLHandleSource/CURLHandleTests/CURLHandleTests.m
+++ CURLHandleSource/CURLHandleTests/CURLHandleTests.m
@@ -10,7 +10,11 @@
 
 #import <SenTestingKit/SenTestingKit.h>
 
-@interface CURLHandleTests : SenTestCase
+@interface CURLHandleTests : SenTestCase<CURLHandleDelegate>
+
+@property (strong, nonatomic) NSData* data;
+@property (strong, nonatomic) NSURLResponse* response;
+@property (assign, nonatomic) BOOL sending;
 
 @end
 
@@ -30,9 +34,40 @@
     [super tearDown];
 }
 
-- (void)testSimpleHandle
+- (void)handle:(CURLHandle *)handle didReceiveData:(NSData *)data
+{
+    self.data = data;
+}
+
+- (void)handle:(CURLHandle *)handle didReceiveResponse:(NSURLResponse *)response
+{
+    self.response = response;
+}
+
+- (void)handle:(CURLHandle *)handle willSendBodyDataOfLength:(NSUInteger)bytesWritten
+{
+    self.sending = YES;
+}
+
+- (void)handle:(CURLHandle *)handle didReceiveDebugInformation:(NSString *)string ofType:(curl_infotype)type
+{
+    CURLHandleLog(@"got debug info: %@ type:%d", string, type);
+}
+
+
+- (void)testSimpleDownload
 {
     CURLHandle* handle = [[CURLHandle alloc] init];
+    handle.delegate = self;
+
+    NSError* error = nil;
+    NSURLRequest* request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://www.karelia.com"]];
+
+    BOOL ok = [handle loadRequest:request error:&error];
+    STAssertTrue(ok, @"failed to load request, with error %@", error);
+
+    STAssertNotNil(self.response, @"got no response");
+    STAssertTrue([self.data length] > 0, @"got no data");
 
     [handle release];
 }
