4effe85feaae6767598a98737c23a180554504ab
Sam Deane <sam@elegantchaos.com>
2013-04-01 14:45:34 +0100

    Added more standalone tests.

--- CURLHandleSource/CURLHandleTests/StandaloneGcdTest.m
+++ CURLHandleSource/CURLHandleTests/StandaloneGcdTest.m
@@ -21,30 +21,41 @@
 @implementation StandaloneGCDTest
 
 
-#define log_message(...) fprintf(stderr, __VA_ARGS__)
+#define log_normal(...) fprintf(stderr, __VA_ARGS__)
 #define log_error(...) fprintf(stderr, "ERROR: " __VA_ARGS__)
+//#define log_detail(...) fprintf(stderr, __VA_ARGS__)
+#define log_detail(...)
 
-dispatch_queue_t queue;
-int remaining = 0;
-CURLM *curl_handle;
-dispatch_source_t timeout;
+static void add_download(const char *url);
 
-#pragma mark - Socket context
+static dispatch_queue_t queue;
+static int remaining = 0;
+static int repeats = 20;
+static CURLM *curl_handle;
+static dispatch_source_t timeout;
 
-typedef struct socket_context_s {
+typedef struct curl_handle_context_s {
+    CURL *handle;
+    struct curl_slist* post_commands;
+    const char *full_url;
+} curl_handle_context_s;
+
+typedef struct curl_socket_context_s {
     dispatch_source_t read_source;
     dispatch_source_t write_source;
-} socket_context_t;
+} curl_context_t;
 
-socket_context_t* create_socket_context_s()
+static curl_context_t* create_curl_context()
 {
-    socket_context_t *context = (socket_context_t *) malloc(sizeof *context);
-    memset(context, 0, sizeof(socket_context_t));
+    curl_context_t *context = (curl_context_t *) malloc(sizeof *context);
+    memset(context, 0, sizeof(curl_context_t));
+
+    log_detail("created context %p\n", context);
 
     return context;
 }
 
-void destroy_socket_context_s(socket_context_t *context)
+static void destroy_curl_context(curl_context_t *context)
 {
     if (context->read_source)
         dispatch_source_cancel(context->read_source);
@@ -52,12 +63,14 @@ void destroy_socket_context_s(socket_context_t *context)
     if (context->write_source)
         dispatch_source_cancel(context->write_source);
 
+    log_detail("destroyed context %p\n", context);
+
     free(context);
 }
 
-#pragma mark - Socket Action
+#pragma mark - socket action support
 
-void curl_perform_action(int socket, int actions)
+static void curl_perform_action(int socket, int actions)
 {
     int running_handles;
     char *done_url;
@@ -72,12 +85,23 @@ void curl_perform_action(int socket, int actions)
             {
                 CURL* easy = message->easy_handle;
                 curl_easy_getinfo(easy, CURLINFO_EFFECTIVE_URL, &done_url);
+                curl_handle_context_s* context = NULL;
+                curl_easy_getinfo(easy, CURLINFO_PRIVATE, &context);
                 CURLcode code = message->data.result;
-                printf("%s DONE\ncode:%d - %s\n", done_url, code, curl_easy_strerror(code));
+                printf("%s DONE\ncode:%d - %s\nremaining:%d\n", done_url, code, curl_easy_strerror(code), --remaining);
+                curl_slist_free_all(context->post_commands);
+
+                if (--repeats)
+                {
+                    const char* full_url = context->full_url;
+                    add_download(full_url);
+                }
+
+                free(context);
+
 
                 curl_multi_remove_handle(curl_handle, message->easy_handle);
                 curl_easy_cleanup(message->easy_handle);
-                --remaining;
 
                 break;
             }
@@ -88,16 +112,24 @@ void curl_perform_action(int socket, int actions)
     }
 }
 
+static const char* action_name(int action)
+{
+    return action == CURL_CSELECT_IN ? "read" : "write";
+}
+
 #pragma mark - GCD utilities
 
 
-dispatch_source_t create_source(dispatch_source_type_t type, int socket, int action)
+static dispatch_source_t create_source(dispatch_source_type_t type, int socket, int action)
 {
+    log_detail("make source socket %d action %s\n", socket, action_name(action));
     dispatch_source_t source = dispatch_source_create(type, socket, 0, queue);
     dispatch_source_set_event_handler(source, ^{
+        log_detail("source event socket %d action %s\n", socket, action_name(action));
         curl_perform_action(socket, action);
     });
     dispatch_source_set_cancel_handler(source, ^{
+        log_detail("source cancelled socket %d action %s\n", socket, action_name(action));
         dispatch_release(source);
     });
 
@@ -105,7 +137,7 @@ dispatch_source_t create_source(dispatch_source_type_t type, int socket, int act
     return source;
 }
 
-void create_timeout(StandaloneGCDTest* test)
+static void create_timeout()
 {
     timeout = dispatch_source_create(DISPATCH_SOURCE_TYPE_TIMER, 0, 0, queue);
     dispatch_source_set_event_handler(timeout, ^{
@@ -113,17 +145,16 @@ void create_timeout(StandaloneGCDTest* test)
         if (remaining == 0)
         {
             curl_multi_cleanup(curl_handle);
-            [test pause];
+            exit(0);
         }
     });
 
     dispatch_resume(timeout);
 }
 
-
 #pragma mark - MULTI callbacks
 
-void timeout_func(CURLM *multi, long timeout_ms, void *userp)
+static void timeout_func(CURLM *multi, long timeout_ms, void *userp)
 {
     if (timeout_ms <= 0)
         timeout_ms = 1; /* 0 means directly call socket_action, but we'll do it in
@@ -133,13 +164,13 @@ void timeout_func(CURLM *multi, long timeout_ms, void *userp)
     dispatch_source_set_timer(timeout, DISPATCH_TIME_NOW, timeout_ns, timeout_ns / 100);
 }
 
-int multi_socket_func(CURL *easy, curl_socket_t s, int action, void *userp, void *socketp)
+static int multi_socket_func(CURL *easy, curl_socket_t s, int action, void *userp, void *socketp)
 {
-    socket_context_t *curl_context = (socket_context_t*) socketp;
+    curl_context_t *curl_context = (curl_context_t*) socketp;
 
     if (action == CURL_POLL_IN || action == CURL_POLL_OUT) {
         if (!curl_context) {
-            curl_context = create_socket_context_s();
+            curl_context = create_curl_context();
             curl_multi_assign(curl_handle, s, (void *) curl_context);
         }
     }
@@ -155,7 +186,7 @@ int multi_socket_func(CURL *easy, curl_socket_t s, int action, void *userp, void
 
         case CURL_POLL_REMOVE:
             if (curl_context) {
-                destroy_socket_context_s(curl_context);
+                destroy_curl_context(curl_context);
                 curl_multi_assign(curl_handle, s, NULL);
             }
             break;
@@ -168,50 +199,67 @@ int multi_socket_func(CURL *easy, curl_socket_t s, int action, void *userp, void
 
 #pragma mark - Top Level
 
-void add_download(const char *url, int num)
+static void add_download(const char *url)
 {
-    char filename[50];
-    FILE *file;
     CURL *handle;
 
-    sprintf(filename, "%d.download.txt", num);
-
-    file = fopen(filename, "w");
-    if (file == NULL) {
-        log_error("Error opening %s\n", filename);
-        return;
-    }
-
     handle = curl_easy_init();
-    curl_easy_setopt(handle, CURLOPT_WRITEFUNCTION, NULL);
-    curl_easy_setopt(handle, CURLOPT_WRITEDATA, file);
+
+    curl_handle_context_s* context = malloc(sizeof *context);
+    memset(context, 0, sizeof *context);
+    context->handle = handle;
+    context->post_commands = NULL;
+    context->full_url = strdup(url);
+    curl_easy_setopt(handle, CURLOPT_PRIVATE, context);
     curl_easy_setopt(handle, CURLOPT_URL, url);
 
+
+    char randomname[CURL_ERROR_SIZE];
+    char makecmd[CURL_ERROR_SIZE];
+    char chmodcmd[CURL_ERROR_SIZE];
+    char delcmd[CURL_ERROR_SIZE];
+
+    sprintf(randomname, "test-%d", rand());
+
+
+    sprintf(makecmd, "*MKD %s", randomname);
+    sprintf(chmodcmd, "SITE CHMOD 0744 %s", randomname);
+    sprintf(delcmd, "DELE %s", randomname);
+
+    context->post_commands = curl_slist_append(context->post_commands, makecmd);
+    context->post_commands = curl_slist_append(context->post_commands, chmodcmd);
+    context->post_commands = curl_slist_append(context->post_commands, "*DELE file1.txt");
+    context->post_commands = curl_slist_append(context->post_commands, "*DELE file2.txt");
+    context->post_commands = curl_slist_append(context->post_commands, delcmd);
+
+    curl_easy_setopt(handle, CURLOPT_POSTQUOTE, context->post_commands);
+
+    long timeout = 60;
+    curl_easy_setopt(handle, CURLOPT_CONNECTTIMEOUT, timeout);
+
+    curl_easy_setopt(handle, CURLOPT_NOBODY, 1);
+    
     dispatch_async(queue, ^{
         curl_multi_add_handle(curl_handle, handle);
-        log_message("Added download %s\n", url);
+        log_normal("Added download %s\n", url);
         ++remaining;
     });
 }
 
-
 - (void)test_multi_gcd
 {
-    if ([self setupServerWithResponseFileNamed:@"ftp"])
-    {
-        queue = dispatch_queue_create("curl queue", 0);
+    queue = dispatch_queue_create("curl gcd test queue", 0);
 
-        create_timeout(self);
+    create_timeout(self);
 
-        curl_handle = curl_multi_init();
-        curl_multi_setopt(curl_handle, CURLMOPT_SOCKETFUNCTION, multi_socket_func);
-        curl_multi_setopt(curl_handle, CURLMOPT_TIMERFUNCTION, timeout_func);
+    curl_handle = curl_multi_init();
+    curl_multi_setopt(curl_handle, CURLMOPT_SOCKETFUNCTION, multi_socket_func);
+    curl_multi_setopt(curl_handle, CURLMOPT_TIMERFUNCTION, timeout_func);
 
-        NSURL* url = [self URLForPath:@"crashtest"];
-        add_download([[url path] UTF8String], 1);
-        
-        [self runUntilPaused];
-    }
+    NSURL* url = [[self ftpTestServer] URLByAppendingPathComponent:@"multigcd"];
+    add_download([[url absoluteString] UTF8String]);
+    
+    [self runUntilPaused];
 }
 
 @end
---
+++ CURLHandleSource/CURLHandleTests/StandaloneNoGcdTest.m
@@ -0,0 +1,150 @@
+//
+//  CURLMultiTests.m
+//
+//  Created by Sam Deane on 20/09/2012.
+//  Copyright (c) 2012 Karelia Software. All rights reserved.
+//
+
+#import "CURLHandleBasedTest.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+#include <string.h>
+
+#include <dispatch/dispatch.h>
+
+@interface StandaloneNoGCDTest : CURLHandleBasedTest
+
+@end
+
+@implementation StandaloneNoGCDTest
+
+#define log_message(...) fprintf(stderr, __VA_ARGS__)
+#define log_error(...) fprintf(stderr, "ERROR: " __VA_ARGS__)
+
+void add_download(const char *url);
+
+
+int remaining = 0;
+int repeats = 20;
+CURLM *curl_handle;
+
+void curl_perform_wait()
+{
+    long timeout_ms = -1;
+    CURLMcode result = curl_multi_timeout(curl_handle, &timeout_ms);
+    if (result != CURLM_OK) log_error("curl_multi_timeout error %d", result);
+
+    if (timeout_ms < 1) timeout_ms = 1;
+
+    int numfds = 0;
+    result = curl_multi_wait(curl_handle, NULL, 0, (int)timeout_ms, &numfds);
+    if (result != CURLM_OK) log_error("curl_multi_wait error %d", result);
+
+    int numrunning = 0;
+    result = curl_multi_perform(curl_handle, &numrunning);
+    if (result != CURLM_OK) log_error("curl_multi_perform error %d", result);
+
+    int pending = 0;
+    CURLMsg *message;
+    while ((message = curl_multi_info_read(curl_handle, &pending))) {
+        switch (message->msg) {
+            case CURLMSG_DONE:
+            {
+                const char* done_url;
+                CURL* easy = message->easy_handle;
+                curl_easy_getinfo(easy, CURLINFO_EFFECTIVE_URL, &done_url);
+                CURLcode code = message->data.result;
+                printf("%s DONE\ncode:%d - %s\n", done_url, code,
+                       curl_easy_strerror(code));
+
+                struct curl_slist* list;
+                curl_easy_getinfo(easy, CURLINFO_PRIVATE, &list);
+
+                --remaining;
+
+                if (--repeats)
+                {
+                    add_download(done_url);
+                }
+
+                curl_multi_remove_handle(curl_handle, easy);
+                curl_easy_cleanup(easy);
+                curl_slist_free_all(list);
+
+                break;
+            }
+            default:
+                log_error("CURLMSG default\n");
+                abort();
+        }
+    }
+}
+
+
+int debug_func(CURL *curl, curl_infotype infoType, char *info, size_t infoLength, void *userp)
+{
+    char* string = strndup(info, infoLength);
+    fprintf(stderr, "debug %d: %s", infoType, string);
+    free(string);
+    return 0;
+}
+
+void add_download(const char *url)
+{
+    CURL *handle = curl_easy_init();
+    curl_easy_setopt(handle, CURLOPT_URL, url);
+    curl_easy_setopt(handle, CURLOPT_DEBUGFUNCTION, debug_func);
+    curl_easy_setopt(handle, CURLOPT_VERBOSE, 1);
+
+    curl_easy_setopt(handle, CURLOPT_URL, url);
+
+
+    char randomname[CURL_ERROR_SIZE];
+    char makecmd[CURL_ERROR_SIZE];
+    char chmodcmd[CURL_ERROR_SIZE];
+    char delcmd[CURL_ERROR_SIZE];
+
+    sprintf(randomname, "test-%d", rand());
+
+
+    sprintf(makecmd, "*MKD %s", randomname);
+    sprintf(chmodcmd, "SITE CHMOD 0744 %s", randomname);
+    sprintf(delcmd, "DELE %s", randomname);
+
+    struct curl_slist* list = curl_slist_append(NULL, makecmd);
+    list = curl_slist_append(list, chmodcmd);
+    list = curl_slist_append(list, "*DELE file1.txt");
+    list = curl_slist_append(list, "*DELE file2.txt");
+    list = curl_slist_append(list, delcmd);
+    curl_easy_setopt(handle, CURLOPT_PRIVATE, list);
+    curl_easy_setopt(handle, CURLOPT_POSTQUOTE, list);
+
+    long timeout = 60;
+    curl_easy_setopt(handle, CURLOPT_CONNECTTIMEOUT, timeout);
+
+    curl_easy_setopt(handle, CURLOPT_NOBODY, 1);
+
+    ++remaining;
+    curl_multi_add_handle(curl_handle, handle);
+    log_message("Added download %s\n", url);
+}
+
+- (void)test_multi_no_gcd
+{
+    if ([self setupServerWithResponseFileNamed:@"ftp"])
+    {
+        curl_handle = curl_multi_init();
+
+        NSURL* url = [[self ftpTestServer] URLByAppendingPathComponent:@"multinogcd"];
+        add_download([[url absoluteString] UTF8String]);
+
+        while(remaining > 0)
+            curl_perform_wait(self);
+
+        curl_multi_cleanup(curl_handle);
+    }
+}
+
+@end
