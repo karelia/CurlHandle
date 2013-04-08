99250987d98de5c0407fe3175216faf59ef3c92a
Sam Deane <sam@elegantchaos.com>
2013-04-08 12:05:10 +0100

    Put temporary build products into ~/Libraries/Caches when building libcurl from script. They aren't essential, but ideally we want them to hang around for debugging purposes.

--- build-libs.sh
+++ build-libs.sh
@@ -2,13 +2,15 @@ build() {
 
 	echo "Building $1"
 
-	xcodebuild -project $1.xcodeproj -target $2 -configuration Debug > /tmp/build.log
+	obj="$HOME/Library/Caches/CurlHandle/obj"
+	sym="$HOME/Library/Caches/CurlHandle/sym"
+	xcodebuild -project $1.xcodeproj -target $2 -configuration Debug OBJROOT="$obj" SYMROOT="$sym" > /tmp/build.log 
 	res=$?
 
 	if [ $res -ne 0 ];
 	then
 		cat /tmp/build.log
-		echo '$1 build failed'
+		echo "$1 build failed"
 		exit $res
 	fi
 
