2cbce7e754d58c713da9ce5eae02d03cd9f970c0
Sam Deane <sam@elegantchaos.com>
2013-04-10 23:50:12 +0100

    Update multi documentation.

--- CURLHandleSource/CURLMulti.h
+++ CURLHandleSource/CURLMulti.h
@@ -100,6 +100,18 @@
 
 - (void)stopManagingHandle:(CURLHandle*)handle;
 
+/**
+ Update the dispatch source for a given socket and type.
+ 
+ @warning The routine is used internally by <CURLMulti> / <CURLSocket>, and shouldn't be called from your code.
+
+ @param source The current dispatch source for the given type
+ @param type Is this the source for reading or writing?
+ @param socket The <CURLSocket> object that owns the source.
+ @param required Is the source required? If not, an existing source will be cancelled. If required and the source parameter is nil, and new one will be created.
+ @return The new/updated dispatch source.
+*/
+
 - (dispatch_source_t)updateSource:(dispatch_source_t)source type:(dispatch_source_type_t)type socket:(CURLSocket*)socket raw:(int)raw required:(BOOL)required;
 
 @end
