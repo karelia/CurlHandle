b0d2211eedb5796bde4c1e2b4664bd37aa7a48fa
Sam Deane <sam@elegantchaos.com>
2013-04-10 23:49:40 +0100

    Update multi support documentation.

--- CURLHandleSource/CURLHandle+MultiSupport.h
+++ CURLHandleSource/CURLHandle+MultiSupport.h
@@ -6,15 +6,46 @@
 
 #import "CURLHandle.h"
 
+
 /**
  Private API used by CURLMulti.
- Not exported in the framework.
+ Not exported in the framework, and not recommended for general use.
  */
 
 @interface CURLHandle(MultiSupport)
-- (CURL *) curl;
+
+/** @name Internal Methods */
+
+/**
+ The CURL handle managed by this object.
+ */
+
+- (CURL*)curl;
+
+/**
+ Called by <CURLMulti> to tell the handle that it has completed.
+ 
+ @param code The completion code.
+ @param isMultiCode Is the code a result from the handle itself, or the multi managing it?
+ */
+
 - (void)completeWithCode:(NSInteger)code isMulti:(BOOL)isMultiCode;
+
+/**
+ Has the handle completed?
+ 
+ @return YES if the handle has completed.
+ */
+
 - (BOOL)hasCompleted;
+
+/**
+ Called by <CURLMulti> when it stops managing the handle.
+ 
+ @param multi The multi object that was managing this handle.
+ */
+
 - (void)removedByMulti:(CURLMulti*)multi;
+
 @end
 
