20378fa657d5cf18b6e13ebabb0b64c301770492
Sam Deane <sam@elegantchaos.com>
2013-04-11 11:52:54 +0100

    Generalised curl rename install script to take architecture as a parameter

--- Scripts/curl-rename-install.sh
+++ Scripts/curl-rename-install.sh
@@ -1,9 +1,12 @@
+# First parameter should be "i386" or "x86_64"
+MODE=$1
+
 # glibtoolize (and maybe other tools) are not supplied with OS X.
 # Add default macports & homebrew paths in attempt to find them.
 export PATH=${PATH}:/opt/local/bin:/usr/local/bin
 
 # Correct the load commands of the dylibs.
-cd "${OBJROOT}/curl-i386/lib/.libs"
+cd "${OBJROOT}/curl-$MODE/lib/.libs"
 # Get the full filenames of the dylibs from the symlinks.
 # E.g.: dylib:"libcrypto.1.0.0.dylib" symlink:"libcrypto.dylib"
 LONG_DYLIB=`readlink -n libcurl.dylib`
@@ -15,4 +18,4 @@ install_name_tool -id @rpath/libcurl.dylib ${LONG_DYLIB}
 install_name_tool -add_rpath @loader_path/../Frameworks ${LONG_DYLIB}
 
 # Copy dylibs to have arch in name.
-cp -f ${LONG_DYLIB} libcurl-i386.dylib
+cp -f ${LONG_DYLIB} libcurl-$MODE.dylib
