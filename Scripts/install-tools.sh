0cb9f9ff96ba9874c9339de42dcbf0d1e973fbfd
Sam Deane <sam@elegantchaos.com>
2013-04-22 11:42:30 +0100

    Add script to install required tools.

---
+++ Scripts/install-tools.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+brew update
+brew install automake
+brew versions automake
+cd /usr/local/Library/Formula/
+git checkout 3a7567c /usr/local/Library/Formula/automake.rb
+brew unlink automake
+brew install automake
