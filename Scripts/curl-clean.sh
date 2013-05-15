8bf76300225474e355fd166d1e5a3d1190c296a7
Sam Deane <sam@elegantchaos.com>
2013-05-15 16:38:11 +0100

    Fix cleanup of old output.

--- Scripts/curl-clean.sh
+++ Scripts/curl-clean.sh
@@ -2,15 +2,10 @@
 # Add default macports & homebrew paths in attempt to find them.
 export PATH=${PATH}:/opt/local/bin:/usr/local/bin
 
-# Clean original source dir, just in case.
-cd "${SRCROOT}/../curl"
-echo "Please ignore any messages about \"No rule to make target distclean.\" That just means the build dir is already clean."
-make distclean
-
 # Remove final output files from previous run.
-cd "${SRCROOT}"
-rm -f  "libcurl.dylib"
-rm -Rf "libcurl.dylib.dSYM"
+OUTDIR="${SRCROOT}/built"
+rm -f  "${OUTDIR}/libcurl.dylib"
+rm -Rf "${OUTDIR}/libcurl.dylib.dSYM"
 
 # Remove intermediate output files from previous run.
 rm -f  "${OBJROOT}/libcurl.dylib"
