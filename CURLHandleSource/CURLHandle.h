4c2966bcbb027aa751bc66aa527bea5a61391c10
Mike Abdullah <me@mikeabdullah.net>
2013-06-19 15:48:09 +0100

    CURLHandle.h public header

---
+++ CURLHandleSource/CURLHandle.h
@@ -0,0 +1,13 @@
+//
+//  CURLHandle.h
+//  CURLHandle
+//
+//  Created by Mike on 19/06/2013.
+//  Copyright (c) 2013 Karelia Software. All rights reserved.
+//
+
+
+#import <CURLHandle/CURLTransfer.h>
+#import <CURLHandle/NSURLRequest+CURLHandle.h>
+#import <CURLHandle/CURLProtocol.h>
+#import <CURLHandle/CK2SSHCredential.h>
